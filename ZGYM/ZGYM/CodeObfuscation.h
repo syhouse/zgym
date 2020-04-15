577e67533a4f4a6f1ca1281c8e507273523eeceb
遥石 <shiyao357@gmail.com>
2020-04-15 15:59:29 +0800

    教育bug调整  首页审核UI调整

--- ZGYM/ZGYM/CodeObfuscation.h
+++ ZGYM/ZGYM/CodeObfuscation.h
@@ -1,10 +1,10 @@
 #ifndef CodeObfuscation_h
 #define Demo_codeObfuscation_h
-//confuse string at Wed Apr 15 15:43:18 CST 2020
-#define detailModel IwvCETvILPBsRvYX
-#define homeModel zhhhbFpeiplGHJto
-#define messageModel KpLyGuCkJcYnHvHO
-#define deModel QxZWwVufCtDYXuWF
-#define curruntIndexPath UGGAGijpCmVOANfF
-#define refreshData GEoqqIblULMXSCRY
+//confuse string at Wed Apr 15 15:55:05 CST 2020
+#define detailModel UOxdcpNpNJWaUzkz
+#define homeModel tWBsZUNIgJwoIiTj
+#define messageModel soBJXGHCuTxYwiXT
+#define deModel emCzfrjlBOCwDpgt
+#define curruntIndexPath LaQAmycuTYDRjLXy
+#define refreshData ALufjulXURdhfFbe
 #endif
