20d3930abb1dc4c7dc42a879a3a0afdc41d9617d
Liu Jie <luemark@live.com>
2020-04-15 17:16:47 +0800

    commit

--- ZGYM/ZGYM/CodeObfuscation.h
+++ ZGYM/ZGYM/CodeObfuscation.h
@@ -1,10 +1,10 @@
 #ifndef CodeObfuscation_h
 #define Demo_codeObfuscation_h
-//confuse string at Wed Apr 15 16:36:43 CST 2020
-#define detailModel bBplEsCghMWOPXuv
-#define homeModel ECfkWCcPhJQoeyRN
-#define messageModel zKoBmLXgROZyxdgz
-#define deModel GQJYmvxruMztzkIQ
-#define curruntIndexPath TdDTqVGxhvkzweJc
-#define refreshData KVRtAhCjmsquHSyS
+//confuse string at Wed Apr 15 16:42:04 CST 2020
+#define detailModel wRVWKErojavoVeDA
+#define homeModel sQFtmsQDtIbbnpmQ
+#define messageModel EAgmTVUmsOKkcuMm
+#define deModel ZiBkwPMDdYmofQyK
+#define curruntIndexPath NladXLaHkUDrUNZz
+#define refreshData fJNJwKSSmrtIRbom
 #endif
