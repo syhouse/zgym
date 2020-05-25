2e9c7b3c765651dd5a2bf30cf9c8df60aa791633
Liu Jie <luemark@live.com>
2020-05-25 17:18:03 +0800

    3664修改私聊语音UI

--- ZGYM/ZGYM/YXSSDK/TencentIM/TUIKit/Classes/Section/Chat/Cell/TUIVoiceMessageCell.m
+++ ZGYM/ZGYM/YXSSDK/TencentIM/TUIKit/Classes/Section/Chat/Cell/TUIVoiceMessageCell.m
@@ -23,13 +23,16 @@
         [self.bubbleView addSubview:_voice];
 
         _duration = [[UILabel alloc] init];
-        _duration.font = [UIFont systemFontOfSize:12];
-        _duration.textColor = [UIColor grayColor];
+//        _duration.font = [UIFont systemFontOfSize:12];
+//        _duration.textColor = [UIColor grayColor];
+        _duration.textColor = [UIColor blackColor];
+        _duration.font = [UIFont systemFontOfSize:15];
         [self.bubbleView addSubview:_duration];
 
         _voiceReadPoint = [[UIImageView alloc] init];
         _voiceReadPoint.backgroundColor = [UIColor redColor];
-        _voiceReadPoint.frame = CGRectMake(0, 0, 5, 5);
+//        _voiceReadPoint.frame = CGRectMake(0, 0, 5, 5);
+        _voiceReadPoint.frame = CGRectMake(0, 0, 10, 10);
         _voiceReadPoint.hidden = YES;
         [_voiceReadPoint.layer setCornerRadius:_voiceReadPoint.frame.size.width/2];
         [_voiceReadPoint.layer setMasksToBounds:YES];
@@ -78,20 +81,26 @@
 {
     [super layoutSubviews];
     
-    self.duration.mm_sizeToFitThan(10,TVoiceMessageCell_Duration_Size.height).mm__centerY(self.bubbleView.mm_h/2 - 1);
+//    self.duration.mm_sizeToFitThan(10,TVoiceMessageCell_Duration_Size.height).mm__centerY(self.bubbleView.mm_h/2);
+    
+    self.duration.mm_sizeToFitThan(10,TVoiceMessageCell_Duration_Size.height).mm__centerY(self.bubbleView.mm_h/2 - 6);
     
     self.voice.mm_sizeToFit().mm_top(self.voiceData.voiceTop);
     
     if (self.voiceData.direction == MsgDirectionOutgoing) {
+        //消息发送
         self.bubbleView.mm_left(self.duration.mm_w).mm_flexToRight(0);
-        self.duration.mm_left(-self.duration.mm_w);
         self.voice.mm_right(self.voiceData.cellLayout.bubbleInsets.right);
+        self.duration.mm_left(self.voice.mm_x - self.voice.mm_w - 4);
         self.voiceReadPoint.hidden = YES;
+        
     } else {
+        //消息接收
         self.bubbleView.mm_left(0).mm_flexToRight(self.duration.mm_w);
-        self.duration.mm_right(-self.duration.mm_w);
         self.voice.mm_left(self.voiceData.cellLayout.bubbleInsets.left);
+        self.duration.mm_left(self.voice.mm_x + self.voice.mm_w + 4);
         self.voiceReadPoint.mm_bottom(self.duration.mm_y + self.duration.mm_h).mm_left(self.duration.mm_x);
+        self.voiceReadPoint.mm__centerY(self.bubbleView.mm_h/2 - 4).mm_right(-self.voiceReadPoint.mm_w);
     }
 }
 
