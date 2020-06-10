de84cace72c4dc8823d4deece759f9af2bdbd370
遥石 <shiyao357@gmail.com>
2020-06-10 16:33:24 +0800

    二维码闪光灯位置调整 打卡评论、优成长评论最后一行圆角不展示修复

--- ZGYM/ZGYM/YXSExtension/UIView+YXSCore.swift
+++ ZGYM/ZGYM/YXSExtension/UIView+YXSCore.swift
@@ -25,6 +25,7 @@ extension UIView{
      *  @param corners 需要设置为圆角的角 UIRectCornerTopLeft | UIRectCornerTopRight | UIRectCornerBottomLeft | UIRectCornerBottomRight | UIRectCornerAllCorners
      *  @param radii   需要设置的圆角大小 例如 CGSizeMake(20.0f, 20.0f)
      *  @param rect    需要设置的圆角view的rect
+     *  注意rect 宽度高度和view的实际宽高一致  否则无效(没有效果请先检查展示的宽高是否和设置的一致)
      */
     func yxs_addRoundedCorners(corners: UIRectCorner, radii: CGSize, rect: CGRect) {
         let rounded = UIBezierPath.init(roundedRect: rect, byRoundingCorners: corners, cornerRadii: radii)
